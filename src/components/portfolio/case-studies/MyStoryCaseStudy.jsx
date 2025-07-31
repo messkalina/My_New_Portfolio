9c61a2e69a401b8b0e04423afc4ccdf90d27b695
messkalina <136571919+messkalina@users.noreply.github.com>
2025-07-31 08:42:00 +0200

    Add new images for Orbit case study and update image source in MyStoryCaseStudy component

--- src/components/portfolio/case-studies/MyStoryCaseStudy.jsx
+++ src/components/portfolio/case-studies/MyStoryCaseStudy.jsx
@@ -335,13 +335,13 @@ const MyStoryCaseStudy = () => {
               <div className="right">
                 <div data-aos="fade-up" data-aos-duration="1200">
                   <img
-                    src="/img/portfolio/case-study/site_map2.png"
+                    src="/img/portfolio/case-study/orbit_persona_patient_journey_vertical.png"
                     alt="Wireframe"
                     className="clickable-image"
                     style={{ width: "100%", cursor: "pointer" }}
                     onClick={() =>
                       handleImageClick(
-                        "/img/portfolio/case-study/site_map2.png"
+                        "/img/portfolio/case-study/orbit_persona_patient_journey_vertical.png"
                       )
                     }
                   />
