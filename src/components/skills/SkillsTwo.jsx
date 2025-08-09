5e12b332b14cd54d37148ad9fdf3db90c997e548
messkalina <136571919+messkalina@users.noreply.github.com>
2025-08-09 18:38:14 +0200

    Add skills icons to SkillsTwo component and adjust layout

--- src/components/skills/SkillsTwo.jsx
+++ src/components/skills/SkillsTwo.jsx
@@ -6,12 +6,69 @@ const SkillsTwo = () => {
       <div className="shane_tm_section">
         <div className="shane_tm_skills">
           <div className="container">
-            <div style={{ textAlign: "center" }}>
+            <div style={{ textAlign: "center", marginBottom: "40px" }}>
               <h3>My Skills</h3>
             </div>
-            
+
+            {/* Skills Icons */}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                flexWrap: "wrap",
+                gap: "40px",
+              }}
+            >
+              {/* Skill 1 */}
+              <div style={{ textAlign: "center", width: "150px" }}>
+                <div style={{ marginBottom: "15px" }}>
+                  <img
+                    src="/img/svg/browser-ui-svgrepo-com.svg"
+                    alt="UX Design"
+                    style={{ height: "60px", width: "60px" }}
+                  />
+                </div>
+                <h4 style={{ margin: "0", fontSize: "18px" }}>UX Design</h4>
+              </div>
+
+              {/* Skill 2 */}
+              <div style={{ textAlign: "center", width: "150px" }}>
+                <div style={{ marginBottom: "15px" }}>
+                  <img
+                    src="/img/svg/vector-svgrepo-com.svg"
+                    alt="UI Design"
+                    style={{ height: "60px", width: "60px" }}
+                  />
+                </div>
+                <h4 style={{ margin: "0", fontSize: "18px" }}>UI Design</h4>
+              </div>
+
+              {/* Skill 3 */}
+              <div style={{ textAlign: "center", width: "150px" }}>
+                <div style={{ marginBottom: "15px" }}>
+                  <img
+                    src="/img/svg/code-block-svgrepo.svg"
+                    alt="Frontend Development"
+                    style={{ height: "60px", width: "60px" }}
+                  />
+                </div>
+                <h4 style={{ margin: "0", fontSize: "18px" }}>Basic Frontend</h4>
+              </div>
+
+              {/* Skill 4 */}
+              <div style={{ textAlign: "center", width: "150px" }}>
+                <div style={{ marginBottom: "15px" }}>
+                  <img
+                    src="/img/svg/animation-svgrepo-com.svg"
+                    alt="Motion Design"
+                    style={{ height: "60px", width: "60px" }}
+                  />
+                </div>
+                <h4 style={{ margin: "0", fontSize: "18px" }}>Motion Design</h4>
+              </div>
+            </div>
           </div>
-          {/* End .conainer */}
+          {/* End .container */}
         </div>
       </div>
     </>
