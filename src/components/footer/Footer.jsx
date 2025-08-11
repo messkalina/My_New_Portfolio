3f118b93ffd7ffd2db115f854d59376c49ceb141
messkalina <136571919+messkalina@users.noreply.github.com>
2025-08-11 10:23:49 +0200

    Refactor Footer component to improve icon styling and layout

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Social from "../Social";
 
 const Footer = () => {
   return (
@@ -9,15 +10,18 @@ const Footer = () => {
           <div className="container">
             <div className="inner">
               <div className="">
-              
+                {/* Invert icon colors using a wrapper with CSS filter */}
+                <div style={{ filter: "invert(1)", marginBottom: "16px" }}>
+                  <Social />
+                </div>
                 <div className="footer_links">
                   <Link to="/privacy-policy">Privacy Policy</Link>
-                  <span style={{ margin: "0 16px" }}>|</span>
+                  <span style={{ margin: "0px 16px" }}>|</span>
                   <Link to="/impressum">Impressum</Link>
-                    <p  style={{ marginTop: "16px" }}>
-                  &copy; {new Date().getFullYear()} by Kalina Iwaszko. All
-                  rights reserved.
-                </p>
+                  <p style={{ marginTop: "16px" }}>
+                    &copy; {new Date().getFullYear()} by Kalina Iwaszko. All
+                    rights reserved.
+                  </p>
                 </div>
               </div>
             </div>
