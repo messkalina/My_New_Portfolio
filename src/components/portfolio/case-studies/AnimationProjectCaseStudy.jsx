3758970f4ed4752ed69f37687ed90976198a043d
messkalina <136571919+messkalina@users.noreply.github.com>
2025-08-11 13:16:28 +0200

    Refactor AnimationProjectCaseStudy component: remove unused Button import for cleaner code

--- src/components/portfolio/case-studies/AnimationProjectCaseStudy.jsx
+++ src/components/portfolio/case-studies/AnimationProjectCaseStudy.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Button } from "react";
+import React, { useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import Header from "../../header/Header";
 import Footer from "../../footer/Footer";
