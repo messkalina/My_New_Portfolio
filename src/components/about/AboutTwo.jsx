508e1ff74f83390ac2e93c96e2353a8de14ac71a
messkalina <136571919+messkalina@users.noreply.github.com>
2025-08-11 10:38:39 +0200

    Enhance AboutTwo component text for clarity and emphasis on key skills

--- src/components/about/AboutTwo.jsx
+++ src/components/about/AboutTwo.jsx
@@ -34,7 +34,9 @@ const AboutTwo = () => {
                   data-aos-duration="1200"
                 >
                   <span>About Me</span>
-                  <h3>UX/UI & motion graphic designer based in Berlin, Germany</h3>
+                  <h3>
+                    UX/UI & motion graphic designer based in Berlin, Germany
+                  </h3>
                 </div>
                 <div
                   className="text"
@@ -43,20 +45,24 @@ const AboutTwo = () => {
                 >
                   <p>
                     I’m a UX/UI and Motion Designer with a background in visual
-                    storytelling and product design. I create intuitive and
-                    engaging user experiences for web and mobile applications,
-                    combining strong visual design with user research and
-                    accessibility principles.</p>
-                    <p>I bring experience from fast-paced
-                    startups, where I’ve contributed across the entire design
-                    process — from research and wireframing to high-fidelity
-                    prototyping and motion-enhanced UI.</p>
-                    <br />
-                    <p>My background in motion
-                    design helps me shape seamless, expressive interactions. I
-                    also have a solid understanding of frontend technologies
-                    like HTML, CSS, JavaScript, and React, which I use to better
-                    collaborate with developers and design with implementation
+                    storytelling and video production. I create{" "}
+                    <strong>intuitive</strong> and
+                    <strong> engaging user experiences</strong> for web and
+                    mobile applications, combining strong visual design with{" "}
+                    <strong>user research</strong> and accessibility principles.
+                  </p>
+                  <p>
+                    I bring experience from fast-paced startups, where I’ve
+                    contributed across the entire design process — from research
+                    and wireframing to high-fidelity prototyping and
+                    motion-enhanced UI.
+                  </p>
+                  <br />
+                  <p>
+                    {" "}
+                    I also have a solid grasp of <strong>frontend technologies</strong> such as
+                    <strong> HTML, CSS, JavaScript,</strong> and <strong>React</strong>, which helps me collaborate
+                    effectively with developers and design with implementation
                     in mind.
                   </p>
 
