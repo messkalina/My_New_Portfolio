108563bed97cc4a39275695d0c7e6ff65f6d70e8
messkalina <136571919+messkalina@users.noreply.github.com>
2025-08-12 15:32:04 +0200

    Add footer logo with responsive styling and adjust layout

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,21 +7,34 @@ const Footer = () => {
     <>
       <div className="shane_tm_section">
         <div className="shane_tm_copyright">
+           <div className="footer_logo">
+                <img
+                  src="/img/logo/new/light.svg"
+                  alt="Kalina Iwaszko Logo"
+                  className="footer_logo_img"
+                />
+              </div>
           <div className="container">
+            
             <div className="inner">
+             
               <div className="">
-                {/* Invert icon colors using a wrapper with CSS filter */}
-                <div style={{ filter: "invert(1)", marginBottom: "16px" }}>
-                  <Social />
-                </div>
-                <div className="footer_links">
-                  <Link to="/privacy-policy">Privacy Policy</Link>
-                  <span style={{ margin: "0px 16px" }}>|</span>
-                  <Link to="/impressum">Impressum</Link>
-                  <p style={{ marginTop: "16px" }}>
-                    &copy; {new Date().getFullYear()} by Kalina Iwaszko. All
-                    rights reserved.
-                  </p>
+                {/* Add logo in the top left corner */}
+
+                <div className="footer_right">
+                  {/* Invert icon colors using a wrapper with CSS filter */}
+                  <div style={{ filter: "invert(1)", marginBottom: "16px" }}>
+                    <Social />
+                  </div>
+                  <div className="footer_links">
+                    <Link to="/privacy-policy">Privacy Policy</Link>
+                    <span style={{ margin: "0px 16px" }}>|</span>
+                    <Link to="/impressum">Impressum</Link>
+                    <p style={{ marginTop: "16px" }}>
+                      &copy; {new Date().getFullYear()} by Kalina Iwaszko. All
+                      rights reserved.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
