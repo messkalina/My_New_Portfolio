39e81269545e11cad002046556c4111590fa702a
messkalina <136571919+messkalina@users.noreply.github.com>
2025-08-12 11:08:20 +0200

    Add margin-top style to SkillsTwo section for improved layout

--- src/components/skills/SkillsTwo.jsx
+++ src/components/skills/SkillsTwo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SkillsTwo = () => {
   return (
     <>
-      <div className="shane_tm_section">
+      <div className="shane_tm_section" style={{ marginTop: "-100px" }}>
         <div className="shane_tm_skills"
         style={{ backgroundColor: "#fff" }}
         >
