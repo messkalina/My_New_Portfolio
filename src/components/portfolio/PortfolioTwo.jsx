80747e02f0a32c30447a19919c3b236342c85f2a
messkalina <136571919+messkalina@users.noreply.github.com>
2025-08-14 13:06:09 +0200

    Add new images and video for Veterinary Practice case study in PortfolioTwo component

--- src/components/portfolio/PortfolioTwo.jsx
+++ src/components/portfolio/PortfolioTwo.jsx
@@ -27,10 +27,11 @@ const tabListContent = [
         portfolioLink: "/case-study/unlimitix-app",
       },
       {
-        img: "/img/portfolio/case-study/home_screen_team7.png",
+        img: "/img/portfolio/case-study/vetpraxis_portfolioItem_iblue2.png",
         title: "Veterinary Practice Website",
         meta: "End-to-end UX/UI & Web Development",
         portfolioLink: "/case-study/vet-website",
+        video: "/img/portfolio/case-study/Home_Desktop.mp4", // You can add the video path here
       },
     ],
   },
