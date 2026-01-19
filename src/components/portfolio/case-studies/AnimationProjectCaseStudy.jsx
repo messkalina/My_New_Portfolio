2141c2eb309bd02c50ddc31a9ef1ebf6db08035d
messkalina <136571919+messkalina@users.noreply.github.com>
2026-01-19 19:47:27 +0100

    Adding visuals to Vet Case Study

--- src/components/portfolio/case-studies/AnimationProjectCaseStudy.jsx
+++ src/components/portfolio/case-studies/AnimationProjectCaseStudy.jsx
@@ -192,9 +192,25 @@ const AnimationProjectCaseStudy = () => {
               </div>
               {/* End right */}
             </div>
+             <div className="content">
+          <div className="shane_tm_section">
+            <div
+              className="full-image-section"
+              style={{ width: "100%" }}
+              data-aos="fade-up"
+              data-aos-duration="1200"
+            >
+              <img
+                src="/img/portfolio/case-study/home_screen_team_big.png"
+                alt="App Screens"
+              />
+            </div>
+          </div>
+        </div>
           </div>
           {/* End container */}
         </div>
+        
       </div>
       {/* Design Process Section - Image left, Text right */}
       <div className="shane_tm_section">
