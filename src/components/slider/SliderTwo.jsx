e3e3ca5a08af228863d4642583f1b25bb984feb8
messkalina <136571919+messkalina@users.noreply.github.com>
2026-04-21 16:29:32 +0200

    hero section centered

--- src/components/slider/SliderTwo.jsx
+++ src/components/slider/SliderTwo.jsx
@@ -22,77 +22,32 @@ const Slider = () => {
 
   return (
     <div className="slider-two">
+      <div className="shane_tm_hero_case_study" id="designtrend-home">
       <div className="shane_tm_hero" id="home" data-style="one">
-        <div className="background">
-          <div
-            className="image"
-            style={{
-              backgroundImage: "url(/img/hero_animation_new.gif)",
-              filter: "grayscale(100%)",
-            }}
-          ></div>
-        </div>
         {/* End .background */}
 
         <div className="container">
           <div className="content">
-            <div className="subheading_wrap">
-              <div className="subheading">
-                <div className="name_wrap">
-                  <h3>
-                    Smart Design
-                    <span> with an edge </span> <span></span>
-                  </h3>
-                </div>
-              </div>
-            </div>
-
-            {/* Mobile-centered content wrapper */}
-            <div className="mobile-centered-content">
-              <div className="name_wrap">
-                <h3
-                  style={{
-                    fontSize: "36px",
-                    letterSpacing: "-0.5px",
-                    fontWeight: 700,
-                    color: isMobile ? "#fff" : "black",
-                    textTransform: "none",
-                  }}
-                  className="mobile-white-text"
-                >
-                  Kalina Iwaszko
-                </h3>
-              </div>
-
-              <div
-                className="job_wrap"
-                style={{ marginTop: "-8px", color: "#6a6969" }}
-              >
-                <span className="job">UI/UX & MOTION DESIGNER</span>
-              </div>
-
-              <button
-                className="shane_tm_button fade-in-delay"
-                style={{
-                  opacity: showButton ? 1 : 0,
-                  transition: "opacity 0.8s ease",
-
-                  border: "none",
-                  cursor: "pointer",
-                  padding: 0,
-                  backgroundColor: "transparent",
-                }}
-                onClick={() => {
-                  document.getElementById("portfolio").scrollIntoView({
-                    behavior: "smooth",
-                  });
-                }}
-              >
-                <span className="button-text">See my work</span>
-              </button>
+            <div
+              className="shane_tm_title"
+              data-aos="fade-up"
+              data-aos-duration="1200"
+            >
+            {/* <div className=".shane_tm_title">
+              <span>UX/UI & Motion Designer </span>
+              </div> */}
+              <h4>UX/UI & Motion Designer</h4>
+              <h5>Kalina Iwaszko</h5>
+              {/* <h6>Smart design with an edge</h6> */}
+              <h1>Bringing ideas to live for web and mobile</h1>
+              
             </div>
           </div>
+          </div>
           {/* End content */}
+          <div>
+            <div></div>
+          </div>
 
           <div className="shane_tm_down loaded">
             <div className="line_wrapper">
