e6a75dba6fcd5ec438ade4e4abddfe40fbd41e7b
messkalina <136571919+messkalina@users.noreply.github.com>
2026-05-04 12:49:35 +0200

    SliderTwo responsiveness issues fixed

--- src/components/slider/SliderTwo.jsx
+++ src/components/slider/SliderTwo.jsx
@@ -7,13 +7,11 @@ const Slider = () => {
   useEffect(() => {
     const timer = setTimeout(() => setShowButton(true), 1200);
 
-    // Handle resize events to update mobile state
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
     };
 
     window.addEventListener("resize", handleResize);
-
     return () => {
       clearTimeout(timer);
       window.removeEventListener("resize", handleResize);
@@ -22,115 +20,100 @@ const Slider = () => {
 
   return (
     <div className="slider-two">
-      <div className="shane_tm_hero_case_study" id="designtrend-home">
-        <div className="shane_tm_hero" id="home">
-          {/* End .background */}
-
-          <div className="container">
-            <div className="content">
-              <div
-                className="shane_tm_title"
-                data-aos="fade-up"
-                data-aos-duration="1200"
-              >
-                {/* <div className=".shane_tm_title">
-              <span>UX/UI & Motion Designer </span>
-              </div> */}
+      <div className="shane_tm_hero" id="home">
+        <div className="container">
+          <div className="content"
+          style={{ paddingTop: isMobile ? "60px" : "90px" }}>
+            <div
+              className="shane_tm_title"
+              data-aos="fade-up"
+              data-aos-duration="1200"
+            >
               <p
                 style={{
                   fontSize: "0.8em",
-                 color: "color: #6a6969",
+                  color: "#6a6969",
                   fontWeight: "500",
-                  lineHeight: "0",
+                  lineHeight: "1.2",
                   letterSpacing: "0.6px",
+                  marginBottom: "10px",
                 }}
               >
                 Portfolio
               </p>
-                <h3>Kalina Iwaszko <span>UX/UI & Motion Designer</span></h3>
-                {/* <h4>UX/UI & Motion Designer</h4> */}
-                <h1>
-                  Bringing{" "}
-                  <span style={{ textTransform: "lowercase" }}>ideas</span> to
-                  life for web and mobile
-                </h1>
-              </div>
-              {!isMobile && (
-                <button
-                  className="shane_tm_button fade-in-delay"
-                  style={{
-                    opacity: showButton ? 1 : 0,
-                    transition: "opacity 0.8s ease",
-                    marginTop: "0rem",
-                    border: "none",
-                    cursor: "pointer",
-                    padding: 0,
-                    backgroundColor: "transparent",
-                  }}
-                  onClick={() => {
-                    document.getElementById("portfolio").scrollIntoView({
-                      behavior: "smooth",
-                    });
-                  }}
-                >
-                  <span className="button-text">See my work </span>
-                  {/* <span className="arrow-icon">↓</span> */}
-                </button>
-              )}
+              <h3>
+                Kalina Iwaszko <span>UX/UI & Motion Designer</span>
+              </h3>
+              <h1>
+                Bringing{" "}
+                <span style={{ textTransform: "lowercase" }}>ideas</span> to
+                life for web and mobile
+              </h1>
             </div>
-            <div className="container">
-              <div className="content">
-                <div data-aos="fade-up" data-aos-duration="1200">
-                  <img
-                    src="/img/new_hero_illustration white_3cm.gif"
-                    alt="hero illustration"
-                    style={{
-                      objectFit: "contain",
-                      width: "100%",
-                      maxHeight: "350px",
-                      display: "block",
-                      margin: "0 auto",
-                    }}
-                  />
-                  {isMobile && (
-                    <button
-                      className="shane_tm_button fade-in-delay"
-                      style={{
-                        opacity: showButton ? 1 : 0,
-                        transition: "opacity 0.8s ease",
-                        marginTop: "1.5rem",
-                        border: "none",
-                        cursor: "pointer",
-                        padding: 0,
-                        backgroundColor: "transparent",
-                        width: "100%",
-                      }}
-                      onClick={() => {
-                        document.getElementById("portfolio").scrollIntoView({
-                          behavior: "smooth",
-                        });
-                      }}
-                    >
-                      <span className="button-text">See my work </span>
-                      {/* <span className="arrow-icon">↓</span> */}
-                    </button>
-                  )}
-                </div>
-              </div>
-            </div>
+            {!isMobile && (
+              <button
+                className="shane_tm_button fade-in-delay"
+                style={{
+                  opacity: showButton ? 1 : 0,
+                  transition: "opacity 0.8s ease",
+                  marginTop: "1rem",
+                  border: "none",
+                  cursor: "pointer",
+                  padding: 0,
+                  backgroundColor: "transparent",
+                }}
+                onClick={() => {
+                  document.getElementById("portfolio").scrollIntoView({
+                    behavior: "smooth",
+                  });
+                }}
+              >
+                <span className="button-text">See my work </span>
+              </button>
+            )}
           </div>
-          {/* End content */}
-          <div>
-            <div></div>
-          </div>
-
-          <div className="shane_tm_down loaded">
-            <div className="line_wrapper">
-              <div className="line"></div>
-            </div>
+          <div className="content" data-aos="fade-up" data-aos-duration="1200">
+            <img
+              src="/img/new_hero_illustration white_3cm.gif"
+              alt="hero illustration"
+              style={{
+                objectFit: "contain",
+                width: "100%",
+                maxHeight: "350px",
+                display: "block",
+                margin: "20px auto 0",
+              }}
+            />
+            {isMobile && (
+              <button
+                className="shane_tm_button fade-in-delay"
+                style={{
+                  opacity: showButton ? 1 : 0,
+                  transition: "opacity 0.8s ease",
+                  marginTop: "1.5rem",
+                  border: "none",
+                  cursor: "pointer",
+                  padding: 0,
+                  backgroundColor: "transparent",
+                  width: "100%",
+                }}
+                onClick={() => {
+                  document.getElementById("portfolio").scrollIntoView({
+                    behavior: "smooth",
+                  });
+                }}
+              >
+                <span className="button-text">See my work </span>
+              </button>
+            )}
           </div>
         </div>
-        {/* End .container */}
+
+        <div className="shane_tm_down loaded">
+          <div className="line_wrapper">
+            <div className="line"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
